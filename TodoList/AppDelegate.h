aacc354b181550f22215f64e46cb7c59bb3aa232
Julien Sarazin <julien.sarazin@me.com>
2014-11-16 20:26:52 +0100

    Initial Commit

---
+++ TodoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TodoList
+//
+//  Created by Julien Sarazin on 16/11/14.
+//  Copyright (c) 2014 Julien Sarazin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TodoList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TodoList
+//
+//  Created by Julien Sarazin on 16/11/14.
+//  Copyright (c) 2014 Julien Sarazin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
